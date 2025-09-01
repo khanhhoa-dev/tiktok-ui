2dd8adb1e1de4709bfe312023bbbf481a13345bd
LêKhánhHòa <mcskhanhhoa@gmail.com>
2025-09-01 12:52:57 +0700

    Cấu hình sử dụng Scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import GlobalStyle from './component/index.jsx';
 import App from './App.jsx';
+import './component/GlobalStyle.scss';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
